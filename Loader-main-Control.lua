b0a1c95652ed3405102aad721398efd66af8538d
xNiKlaSTHx <88712393+xNiKlaSTHx@users.noreply.github.com>
2021-11-30 03:52:43 -0500

    Update Loader-main-Control.lua

--- Loader-main-Control.lua
+++ Loader-main-Control.lua
@@ -1,39 +1,31 @@
-local http_request = http_request;
-if syn then
-http_request = syn.request
-elseif SENTINEL_V2 then
-function http_request(tb)
-return {
-StatusCode = 200;
-Body = request(tb.Url, tb.Method, (tb.Body or ''))
-}
-end
+---_G.Key = "GODHUB-NO1-OLX0-iuyI10L" ///Your Key
+
+
+------- script -------
+
+function scriptkuyrai()
+   print("GOD HUB NO 1") 
 end
 
-local body = http_request({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body;
-local decoded = game:GetService('HttpService'):JSONDecode(body)
-local hwid_list = {"Syn-Fingerprint", "Exploit-Guid", "Proto-User-Identifier", "Sentinel-Fingerprint"};
-local hwid = "";
+----------------------
 
-for i, v in next, hwid_list do
-if decoded.headers[v] then
-hwid = decoded.headers[v];
-break
-end
-end
-
-local a = syn.request({
-    Url = "https://25.18.83.176/server.php?key="..key.."&random="..hwid,
+local server = syn.request({
+    Url = "http://25.18.83.176/server.php?key=".._G.Key,
     Method = "GET"
 })
 
-print(a.Body)
-if a.Body == syn.crypt.base64.encode("KEY PASS")..syn.crypt.base64.encode(syn.crypt.base64.encode(os.time()))..syn.crypt.base64.encode("HWID PASS")..syn.crypt.base64.encode(syn.crypt.base64.encode(os.time())) then
-   loadstring(game:HttpGet("https://og-edition.000webhostapp.com/ScriptGODHUB_hrfy4sd8a0e94hgy1dus4d1j15gdct.lua"))()
-elseif a.Body == syn.crypt.base64.encode("KEY PASS")..syn.crypt.base64.encode(syn.crypt.base64.encode(os.time()))..syn.crypt.base64.encode("INVAID HWID")..syn.crypt.base64.encode(syn.crypt.base64.encode(os.time())) then
- game.Players.LocalPlayer:Kick("INVAID HWID")
-elseif a.Body == syn.crypt.base64.encode("INVAID KEY")..syn.crypt.base64.encode(syn.crypt.base64.encode(os.time()))..syn.crypt.base64.encode("INVAID HWID")..syn.crypt.base64.encode(syn.crypt.base64.encode(os.time())) then
-    game.Players.LocalPlayer:Kick("INVAID KEY")
-else
-    game.Players.LocalPlayer:Kick("The database could not be connected")
+local decode = syn_crypt_b64_decode(server.Body)
+
+if decode == "" then
+    print("Invaild Key")
+elseif decode == "Invalid Key" then
+      print("Invaild Key") 
+elseif decode == "Update Hwid" then
+    print("Update Hwid")
+elseif decode == "Invalid HWID" then
+    print("Invalid HWID")
+elseif decode == "You Are Got Blacklist" then
+   print("You Are Got Blacklist")
+elseif decode == "Whitelist" then
+    scriptkuyrai()
 end
