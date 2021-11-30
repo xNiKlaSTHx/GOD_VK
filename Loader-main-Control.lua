23c0c84ca3c5dec5a40939b1d4a07428f6b9d87e
xNiKlaSTHx <88712393+xNiKlaSTHx@users.noreply.github.com>
2021-11-29 21:04:18 -0500

    Update Loader-main-Control.lua

--- Loader-main-Control.lua
+++ Loader-main-Control.lua
@@ -23,7 +23,7 @@ end
 end
 
 local a = syn.request({
-    Url = "https://25.18.83.176/server1.php?key="..key.."&random="..hwid,
+    Url = "https://25.18.83.176/server.php?key="..key.."&random="..hwid,
     Method = "GET"
 })
 
