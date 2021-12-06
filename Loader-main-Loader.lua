a58bf10526a478514da64fa514d38fc82c35af87
xNiKlaSTHx <88712393+xNiKlaSTHx@users.noreply.github.com>
2021-12-06 07:41:50 -0500

    Update Loader-main-Loader.lua

--- Loader-main-Loader.lua
+++ Loader-main-Loader.lua
@@ -1,4 +1,8 @@
 ---Protected By xNiKlaSTHx--- 
+
  loadstring(game:HttpGet("https://og-edition.000webhostapp.com/load-welcome-anti.lua"))()
 wait(5)
+
+-----------------------------
+
 loadstring(game:HttpGet("https://og-edition.000webhostapp.com/ScriptCURSED.HUB_dhkjserthohukolegwer5t4sdhddg56zre.lua"))() ---SCRIPT .HUB
