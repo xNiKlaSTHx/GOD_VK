97739009f62f0cb92deddfdb0e59f4287009c426
xNiKlaSTHx <88712393+xNiKlaSTHx@users.noreply.github.com>
2021-12-06 07:39:34 -0500

    Create Loader-main-Loader.lua

---
+++ Loader-main-Loader.lua
@@ -0,0 +1,4 @@
+---Protected By xNiKlaSTHx--- 
+ loadstring(game:HttpGet("https://og-edition.000webhostapp.com/load-welcome-anti.lua"))()
+wait(5)
+loadstring(game:HttpGet("https://og-edition.000webhostapp.com/ScriptCURSED.HUB_dhkjserthohukolegwer5t4sdhddg56zre.lua"))() ---SCRIPT .HUB
